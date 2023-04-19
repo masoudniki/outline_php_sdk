59a413b408c9cac3d99fccf4017270c763407e27
masoudniki <masoudniki79@gmail.com>
2023-04-19 16:37:28 +0330

    add access key api

--- src/Domains/AccessKey/AccessKey.php
+++ src/Domains/AccessKey/AccessKey.php
@@ -9,6 +9,40 @@ class AccessKey
     public function __construct(
         public Client $client
     ){}
+    public function getListOfAccessKeys(){
+        return $this->client->get("access-keys");
+    }
+    public function getAccessKey($accessKey){
+        return $this->client->get("access-keys/$accessKey");
+    }
+    public function deleteAnAccessKey($accessKey){
+        return $this->client->delete("access-keys/$accessKey");
+    }
+    public function createNewAccessKey(string $method="aes-192-gcm"){
+        return $this->client->post("access-keys",[
+            "json"=>[
+                "method"=>$method
+            ]
+        ]);
+    }
+    public function renameAnAccessKey(string $accessKey,string $newName){
+        return $this->client->put("access-keys/$accessKey/name",[
+            "json"=>[
+                "name"=>$newName
+            ]
+        ]);
+    }
+    public function changeDefaultPortForNewlyCreatedAcessKey($port){
+        return $this->client->put("server/port-for-new-access-keys",[
+            "json"=>[
+                "port"=>$port
+            ]
+        ]);
+    }
+
+
+
+
 
 
 }
